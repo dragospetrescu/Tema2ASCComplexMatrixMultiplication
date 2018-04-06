9a8db6024c9c6007c26d4b90d5e873197854f1c5
dragos <petrescudragos1995@gmail.com>
2018-04-07 01:23:29 +0300

    script to run remote

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-make
-./tema2_neopt input
-make clean
+zip Tema2.zip Makefile* solver_* utils.h main.c input
+scp Tema2.zip dragos.petrescu@fep.grid.pub.ro:ASC/Tema2
+ssh dragos.petrescu@fep.grid.pub.ro ASC/Tema2/run.sh
+rm Tema2.zip
