2f4396eecfb9fa4e77187d9fa47cd92874773756
dragos <petrescudragos1995@gmail.com>
2018-04-07 01:22:55 +0300

    made it c89 compatible

--- solver_neopt.c
+++ solver_neopt.c
@@ -10,8 +10,10 @@
  */
 void print_my2_matrix(double *aux, int N)
 {
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
+	int i, j;
+
+	for (i = 0; i < N; ++i) {
+		for (j = 0; j < N; ++j) {
 			printf("%f+%fi ", aux[2 * (i * N + j)], aux[2 * (i * N + j) + 1]);
 		}
 		printf("\n");
@@ -20,32 +22,30 @@ void print_my2_matrix(double *aux, int N)
 
 double *my_solver(int N, double *A)
 {
-
+	int l, c, i;
 	double *result = malloc(sizeof(double) * 2 * N * N);
 
 
-	for (int l = 0; l < N; ++l) {
-		for (int c = l; c < N; ++c) {
+	for (l = 0; l < N; ++l) {
+		for (c = l; c < N; ++c) {
 			result[2 * (l * N + c)] = 0;
 			result[2 * (l * N + c) + 1] = 0;
 			result[2 * (c * N + l)] = 0;
 			result[2 * (c * N + l) + 1] = 0;
 
-			for (int i = 0; i < N; ++i) {
+			for (i = 0; i < N; ++i) {
 				result[2 * (l * N + c)] +=
 					A[2 * (l * N + i)] * A[2 * (c * N + i)]
 						- A[2 * (l * N + i) + 1] * A[2 * (c * N + i) + 1];
-				result[2 * (l * N + c) + 1] += A[2*(l * N + i)] * A[2*(c * N + i) + 1]
-					+ A[2 * (c * N + i)] * A[2 *(l * N + i) + 1];
+				result[2 * (l * N + c) + 1] +=
+					A[2 * (l * N + i)] * A[2 * (c * N + i) + 1]
+						+ A[2 * (c * N + i)] * A[2 * (l * N + i) + 1];
 			}
-				result[2 * (c * N + l)] = result[2 * (l * N + c)];
-				result[2 * (c * N + l) + 1] = result[2 * (l * N + c) + 1];
+			result[2 * (c * N + l)] = result[2 * (l * N + c)];
+			result[2 * (c * N + l) + 1] = result[2 * (l * N + c) + 1];
 		}
 
 	}
-
-	printf("\n\nResulted Matrix\n");
-	print_my2_matrix(result, N);
 
 	return result;
 }
