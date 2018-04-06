4a308c3f99b8dca45a538bbfd750c1e899f03e90
dragos <petrescudragos1995@gmail.com>
2018-04-07 01:23:13 +0300

    trying opt. needs testing

--- solver_opt.c
+++ solver_opt.c
@@ -8,7 +8,41 @@
 /*
  * Add your optimized implementation here
  */
-double* my_solver(int N, double *A) {
-	printf("OPT SOLVER\n");
-	return NULL;	
+double *my_solver(int N, double *A)
+{
+	int l, c, i;
+	double *result = malloc(sizeof(double) * 2 * N * N);
+
+
+	for (l = 0; l < N; ++l) {
+
+		double *orig_pa = &A[2 * l * N];
+		for (c = l; c < N; ++c) {
+			double *pa = orig_pa;
+			double *pb = &A[2 * c * N];
+			register double real_sum = 0;
+			register double imag_sum = 0;
+			for (i = 0; i < N; ++i) {
+
+				double a1 = *pa;
+				double a2 = *pb;
+				pa++;
+				pb++;
+				double a3 = *pa;
+				double a4 = *pb;
+
+				real_sum += a1 * a2 - a3 * a4;
+				imag_sum += a1 * a3 + a2 * a4;
+				pa++;
+				pb++;
+			}
+			result[2 * (l * N + c)] = real_sum;
+			result[2 * (l * N + c) + 1] = imag_sum;
+			result[2 * (c * N + l)] = real_sum;
+			result[2 * (c * N + l) + 1] = imag_sum;
+		}
+
+	}
+
+	return result;
 }
