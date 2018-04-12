80d8324d313d2b2d2eec9448710cf234cc2d6b91
dragos <petrescudragos1995@gmail.com>
2018-04-12 11:38:54 +0300

    working perfectly for neoptimizat

--- solver_neopt.c
+++ solver_neopt.c
@@ -15,7 +15,10 @@ void print_my2_matrix(double *aux, int N)
 
 	for (i = 0; i < N; ++i) {
 		for (j = 0; j < N; ++j) {
-			printf("%f+%f*i, ", aux[2 * (i * N + j)], aux[2 * (i * N + j) + 1]);
+			if (j == N - 1)
+				printf("%f+%f*i", aux[2 * (i * N + j)], aux[2 * (i * N + j) + 1]);
+			else
+				printf("%f+%f*i, ", aux[2 * (i * N + j)], aux[2 * (i * N + j) + 1]);
 		}
 		printf(";\n");
 	}
@@ -24,13 +27,14 @@ void print_my2_matrix(double *aux, int N)
 double *my_solver(int N, double *A)
 {
 	int l, c, i;
-	double *result = malloc(sizeof(double) * 2 * N * N);
+//	print_my2_matrix(A, N);
+	double *result = calloc(2 * N * N, sizeof(double));
 	for (l = 0; l < N; ++l) {
 		for (c = l; c < N; ++c) {
-			result[2 * (l * N + c)] = 0;
-			result[2 * (l * N + c) + 1] = 0;
-			result[2 * (c * N + l)] = 0;
-			result[2 * (c * N + l) + 1] = 0;
+//			result[2 * (l * N + c)] = 0;
+//			result[2 * (l * N + c) + 1] = 0;
+//			result[2 * (c * N + l)] = 0;
+//			result[2 * (c * N + l) + 1] = 0;
 
 			for (i = 0; i < N; ++i) {
 				result[2 * (l * N + c)] +=
@@ -41,11 +45,13 @@ double *my_solver(int N, double *A)
 					A[2 * (l * N + i)] * A[2 * (c * N + i) + 1]
 						+ A[2 * (c * N + i)] * A[2 * (l * N + i) + 1];
 			}
-			result[2 * (c * N + l)] = result[2 * (l * N + c)];
-			result[2 * (c * N + l) + 1] = result[2 * (l * N + c) + 1];
 		}
 
 	}
+
+//	printf("\n");
+//
+//	print_my2_matrix(result, N);
 
 	return result;
 }
