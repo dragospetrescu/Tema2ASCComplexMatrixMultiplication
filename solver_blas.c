05a3ae8891460484c2ff607422336a4302520f95
dragos <petrescudragos1995@gmail.com>
2018-04-13 22:50:01 +0300

    Working blas

--- solver_blas.c
+++ solver_blas.c
@@ -4,11 +4,22 @@
  * Catalin Olaru / Vlad Spoiala
  */
 #include "utils.h"
-
+#include "cblas.h"
 /* 
  * Add your BLAS implementation here
  */
 double* my_solver(int N, double *A) {
-	printf("BLAS SOLVER\n");
-	return NULL;
+	const enum CBLAS_ORDER Order=CblasRowMajor;
+	const enum CBLAS_UPLO Uplo = CblasUpper;
+	const enum CBLAS_TRANSPOSE Trans = CblasNoTrans;
+
+	double alpha = 1;
+	double beta = 0;
+
+	int s = N;
+	double *result = calloc(2 * N * N, sizeof(double));
+
+	cblas_zsyrk(Order, Uplo, Trans, s, s, &alpha, A, s, &beta, result, N);
+
+	return result;
 }
