16343bfa87e6c5d5d477b45574bf388d7363e1a5
dragos <petrescudragos1995@gmail.com>
2018-04-13 22:53:16 +0300

    updated running and checking scripts

--- checker.sh
+++ checker.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-#make
-#./tema2_neopt input
+
 gcc -g matrix_crawler_n_checker.c -o check -lm
+
+make
+./tema2_neopt input
 
 echo "Neoptimizat"
 for i in {1..5}
@@ -10,4 +12,32 @@ do
     echo -n "Test $i ... "
     ./check ref/"ref$i" "out$i"
 done
+echo ""
+make clean
+rm out*
+
+make
+./tema2_opt_m input
+echo "Optimizat cod"
+for i in {1..5}
+do
+    echo -n "Test $i ... "
+    ./check ref/"ref$i" "out$i"
+done
+echo ""
+make clean
+rm out*
+
+make
+./tema2_opt_f input
+echo "Optimizat flags"
+for i in {1..5}
+do
+    echo -n "Test $i ... "
+    ./check ref/"ref$i" "out$i"
+done
+echo ""
+make clean
+rm out*
+
 rm check
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-zip Tema2.zip Makefile* solver_* utils.h main.c input
+zip Tema2.zip Makefile* solver_* utils.h cblas.h main.c input matrix_crawler_n_checker.c
 scp Tema2.zip dragos.petrescu@fep.grid.pub.ro:ASC/Tema2
-ssh dragos.petrescu@fep.grid.pub.ro ASC/Tema2/run.sh
+#ssh dragos.petrescu@fep.grid.pub.ro ASC/Tema2/run.sh
 rm Tema2.zip
