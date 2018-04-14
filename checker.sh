1996daa524011c9c2ac6028bd27a740805adb174
dragos <petrescudragos1995@gmail.com>
2018-04-15 00:11:05 +0300

    made checker nicer

--- checker.sh
+++ checker.sh
@@ -1,43 +1,104 @@
-#!/usr/bin/env bash
-
-
+module load utilities/intel_parallel_studio_xe_2016
 gcc -g matrix_crawler_n_checker.c -o check -lm
 
+echo "GCC"
+echo ""
+make clean
+rm out*
 make
+echo "Neoptimizat gcc"
 ./tema2_neopt input
-
-echo "Neoptimizat"
 for i in {1..5}
 do
     echo -n "Test $i ... "
     ./check ref/"ref$i" "out$i"
 done
 echo ""
-make clean
 rm out*
 
-make
+
+echo "Optimizat cod gcc"
 ./tema2_opt_m input
-echo "Optimizat cod"
 for i in {1..5}
 do
     echo -n "Test $i ... "
     ./check ref/"ref$i" "out$i"
 done
+echo ""
+rm out*
+
+
+echo "Optimizat flags gcc"
+./tema2_opt_f input
+for i in {1..5}
+do
+    echo -n "Test $i ... "
+    ./check ref/"ref$i" "out$i"
+done
+echo ""
+rm out*
+
+
+echo "BLAS gcc"
+./tema2_blas input
+for i in {1..5}
+do
+    echo -n "Test $i ... "
+    ./check ref/"ref$i" "out$i"
+done
+echo ""
+rm out*
+
+echo "============================"
+echo ""
+
+echo "ICC"
 echo ""
 make clean
 rm out*
-
-make
-./tema2_opt_f input
-echo "Optimizat flags"
+make -f Makefile.icc
+echo "Neoptimizat icc"
+./tema2_neopt input
 for i in {1..5}
 do
     echo -n "Test $i ... "
     ./check ref/"ref$i" "out$i"
 done
 echo ""
-make clean
+rm out*
+
+
+echo "Optimizat cod icc"
+./tema2_opt_m input
+for i in {1..5}
+do
+    echo -n "Test $i ... "
+    ./check ref/"ref$i" "out$i"
+done
+echo ""
+rm out*
+
+
+echo "Optimizat flags icc"
+./tema2_opt_f input
+for i in {1..5}
+do
+    echo -n "Test $i ... "
+    ./check ref/"ref$i" "out$i"
+done
+echo ""
+rm out*
+
+
+echo "BLAS icc"
+./tema2_blas input
+for i in {1..5}
+do
+    echo -n "Test $i ... "
+    ./check ref/"ref$i" "out$i"
+done
+echo ""
 rm out*
 
 rm check
+
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 zip Tema2.zip Makefile* solver_* utils.h cblas.h main.c input matrix_crawler_n_checker.c
 scp Tema2.zip dragos.petrescu@fep.grid.pub.ro:ASC/Tema2
-#ssh dragos.petrescu@fep.grid.pub.ro ASC/Tema2/run.sh
+ssh dragos.petrescu@fep.grid.pub.ro qsub -q ibm-dp.q ASC/Tema2/run.sh
 rm Tema2.zip
